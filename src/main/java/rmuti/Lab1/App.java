d79a15f0798d66243b77115b372a4d7f6ef235cc
ktcdream <mr.dreamreddevil@gmail.com>
2017-02-09 19:41:32 +0700

    first commit

---
+++ src/main/java/rmuti/Lab1/App.java
@@ -0,0 +1,15 @@
+package rmuti.Lab1;
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	 ArrayOperation obj = new ArrayOperation();
+         
+         int sumObj = obj.SumArrays();
+         System.out.println("Sum value is "+sumObj);
+         
+         int maxObj = obj.maxArrays();
+         System.out.println("Max value is "+maxObj);
+
+    }
+}
